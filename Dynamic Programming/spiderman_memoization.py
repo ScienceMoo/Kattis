721ed184046ae671c397dca6996828cb51d20caf
Marie <marie.cornellier@mail.mcgill.ca>
2020-10-29 16:20:25 -0400

    remove old code

--- Dynamic Programming/spiderman_memoization.py
+++ Dynamic Programming/spiderman_memoization.py
@@ -7,34 +7,6 @@ number = int(lines[0])
 
 index = 2
 
-
-class Building:
-	def __init__(self):
-		self.sequence = []
-
-	def find_min(self, current_height, max_height, numbers):
-		if len(numbers) == 1:
-			if current_height - numbers[0] != 0:
-				return 1001
-			else:
-				return max_height
-		else:
-			new_max = max_height
-			if current_height + numbers[0] > max_height:
-				new_max = current_height + numbers[0]
-
-			add_result = 1001
-			if current_height - numbers[0] > 0:
-				add_result = self.find_min(current_height - numbers[0], max_height, numbers[1:])
-			minus_result = self.find_min(current_height + numbers[0], new_max, numbers[1:])
-			if add_result < minus_result:
-				self.sequence.insert(0, "U")
-				return add_result
-			else:
-				self.sequence.insert(0, "D")
-				return minus_result
-
-
 while index < len(lines):
 	distances = list(map(int, lines[index].split()))
 	largest_distance = max(distances)
