e6939005c88271baa334f3a75ef9a09f1dd5f95f
Marie <marie.cornellier@mail.mcgill.ca>
2020-11-14 10:34:55 -0500

    fixed

--- Data Structures Practice/getshorty_Dijkstra.py
+++ Data Structures Practice/getshorty_Dijkstra.py
@@ -1,47 +1,38 @@
 import math
+from queue import PriorityQueue
 
 class Graph:
 	def __init__(self, n, m):
-		self.edges = [[-1 for _ in range(n)] for _ in range(n)]
+		self.edges = [[] for _ in range(n)]
 		for _ in range(m):
 			x, y, f = input().split()
-			self.edges[int(x)][int(y)] = float(f)
-			self.edges[int(y)][int(x)] = float(f)
+			x = int(x)
+			y = int(y)
+			f = float(f)
+			self.edges[x].append([y, f])
+			self.edges[y].append([x, f])
 
 # get first test case
 n, m = map(int, input().split())
 
 while not n == 0:
-	visited = [False] * n
-	remaining = set(range(0, n))
-
-	shortness = [-float('inf')] * n
+	graph = Graph(n, m)
+	visited = set()
+	queue = PriorityQueue()
 
 	# he starts at full height
-	shortness[0] = 0
-	i = 0
-	visited[i] = 0
+	queue.put((-1 * math.log(1), 0))
 
-	graph = Graph(n, m)
-
-	# adjusted the Dijkstra algorithm because we only need 1 path
-	while not visited[n-1]:
-		remaining.remove(i)
-		best = -float('inf')
-		next = -1
-		for j in remaining:
-			if graph.edges[i][j] > 0:
-				shortness[j] = shortness[i] + math.log(graph.edges[i][j])
-			elif graph.edges[i][j] == 0:
-				shortness[j] = -float('inf')
-			if shortness[j] >= best:
-				best = shortness[j]
-				next = j
-		# continue path along best route
-		i = next
-		visited[i] = True
-
-	print("{:.4f}".format(math.exp(shortness[n-1])))
-
-	# next test case
+	while True:
+		if queue.empty():
+			raise Exception('no possible moves')
+		height, intersection = queue.get()
+		visited.add(intersection)
+		if intersection == n - 1:
+			print("{:.4f}".format(math.exp(-1 * height)))
+			break
+		for other, multipler in graph.edges[intersection]:
+			if other not in visited:
+				queue.put((height - math.log(multipler), other))
 	n, m = map(int, input().split())
+
