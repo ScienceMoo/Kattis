237292d86da08f3da5e067d380a1eb32eff0009b
Marie <marie.cornellier@mail.mcgill.ca>
2021-01-12 02:53:16 -0600

    solved trollhunt

---
+++ General Problems/trollhunt.py
@@ -0,0 +1,5 @@
+import math
+
+num_bridges, num_knights, knights_per_group = map(int, input().split())
+num_groups = math.floor(num_knights / knights_per_group)
+print(math.ceil((num_bridges - 1) / num_groups))
